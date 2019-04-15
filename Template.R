cf1399099cf19dc2055b6bfc22afa9f0151df933
Suraj Malpani <31307028+SurajMalpani@users.noreply.github.com>
2019-04-15 15:01:02 -0700

    Import excel file from desktop

--- Template.R
+++ Template.R
@@ -17,6 +17,10 @@ for(p in rqrd_Pkg){
     install.packages(p);
   library(p,character.only = TRUE)
 }
+
+#Choose an excel file to import in R
+Data <- read_excel(file.choose())
+
 #Importing data: readr: read_csv | readxl::read_excel | data.table::fread | gdata::read.xls
 #Functions: purrr : map_dbl, map, map_chr, map2, pmap, invoke_map  | walk, 
 #Cleaning data: tidyr:spread,gather,unite,separate
